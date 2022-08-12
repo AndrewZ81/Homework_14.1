bfc94c837c932bea8223c07e847a2cb44a52ea0a
Andrew Zaiko <andrewzaiko@gmail.com>
2022-08-12 15:25:03 +0300

    Отредактировал "other_dao.py" и "utils.py" для тестирования прочих запросов к БД

--- other_dao.py
+++ other_dao.py
@@ -31,8 +31,8 @@ class OtherDAO:  # Создаём DAO для выборки по прочим к
         """
         db_query = f"SELECT title, listed_in, description " \
                    f"FROM {self.name} " \
-                   f"WHERE type = '{type}'" \
-                   f"AND release_year = '{year}'"
+                   f"WHERE type = '{type}' " \
+                   f"AND release_year = {year}"
         db_data = self.load_database().execute(db_query).fetchall()
         searching_results_as_list = []
         for i in db_data:
@@ -41,3 +41,30 @@ class OtherDAO:  # Создаём DAO для выборки по прочим к
                 i_dict = {"title": i[0], "description": i[2]}
                 searching_results_as_list.append(i_dict)
         return searching_results_as_list
+
+    def search_by_actors(self, actor_1, actor_2):
+        """
+        Находит актёров из БД, игравших с заданными более двух раз
+        :param actor_1: Имя и фамилия актёра 1
+        :param actor_2: Имя и фамилия актёра 2
+        :return: Список актёров (содержит имена и фамилии)
+        """
+        # При составлении запроса регистр ВАЖЕН!
+        db_query = f"SELECT [cast] " \
+                   f"FROM {self.name} " \
+                   f"WHERE [cast] LIKE '%{actor_1}%' " \
+                   f"AND [cast] LIKE '%{actor_2}%'"
+        db_data = self.load_database().execute(db_query).fetchall()
+        searching_results_as_list = []
+        filtered_cast_list = []
+        for i in db_data:
+            cast_list = [k for k in i[0].split(', ')]
+            for m in cast_list:
+                if m != actor_1 and m != actor_2:
+                    filtered_cast_list.append(m)
+        for i in filtered_cast_list:
+            actor_count = filtered_cast_list.count(i)
+            if actor_count > 2:
+                searching_results_as_list.append(i)
+        searching_results_as_unique_list = list(set(searching_results_as_list))
+        return searching_results_as_unique_list
--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 from config import PATH, NAME  # Импортируем константы из конфигурационного файла
 from other_dao import OtherDAO # Импортируем для выборки поиск картин из БД по типу, году выпуска и жанру
 
-
 try:
     file = open(PATH)
 except FileNotFoundError:
@@ -9,10 +8,9 @@ except FileNotFoundError:
 else:
     file.close()
 
-# Вызываем поиск картин из БД по типу, году выпуска и жанру
 database = OtherDAO(PATH, NAME)  # Создаём объект класса
 
-# Создаём тесты
+# Вызываем поиск картин из БД по типу, году выпуска и жанру в виде тестов
 unreal_pictures = database.search_by_type_year_genre("Movie", 1000, "Dramas")
 real_pictures = database.search_by_type_year_genre("Movie", 2000, "Dramas")
 
@@ -24,3 +22,16 @@ except AssertionError:
     raise
 else:
     print("Поиск картин из БД по типу, году выпуска и жанру работает корректно")
+
+# Вызываем поиск акиёров из БД, сыгравших с данными актёрами более двух раз
+real_actors = database.search_by_actors("Rose McIver", "Ben Lamb")
+unreal_actors = database.search_by_actors("Unreal Actor", "Unreal Actor")
+
+try:
+    assert unreal_actors == [], "Не ни одного актёра, сыгравших с данными актёрами более двух раз!"
+    assert real_actors != [], "Есть актёры, сыгравшие с данными актёрами более двух раз!"
+except AssertionError:
+    print("Поиск актёров из БД, сыгравших с данными актёрами более двух раз, работает некорректно")
+    raise
+else:
+    print("Поиск актёров из БД, сыгравших с данными актёрами более двух раз, работает корректно")
